b9b45fea6d35e965c803b3aa7cf0fc384dcdbf16
NareshYadav@45 <chinthalanareshyadav@gmail.com>
2024-03-22 15:31:23 +0530

    high priority task completed

--- MavenWebApp/src/main/java/groupid/App.java
+++ MavenWebApp/src/main/java/groupid/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
+		int i=10;
+		int j=90;
+		
+		System.out.println("high priority task");
+		
+		System.out.println(i+j);
+		
         System.out.println( "Hello World!" );
     }
 }
