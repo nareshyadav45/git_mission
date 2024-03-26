109e8bf3efdced468b10db461459c5fc730ae507
NareshYadav@45 <chinthalanareshyadav@gmail.com>
2024-03-26 19:33:35 +0530

    new changes in own .java

---
+++ MavenWebApp/own.java
@@ -0,0 +1,36 @@
+<<<<<<< Updated upstream
+=======
+//Animals
+//birds
+//trees
+//one
+//two
+//tthree
+//four
+//A
+//B
+//C
+
+
+//E
+//F
+
+
+
+
+
+
+
+//GCMParameterSpe
+//H
+
+//ghmc
+
+//bmc
+
+
+//cmc
+
+
+//gmc
+>>>>>>> Stashed changes
